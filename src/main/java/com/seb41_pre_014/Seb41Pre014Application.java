7bf5fb61c493d2b01894fc62a4717d831b53e0c3
jambin <gksmfcksqls@gmail.com>
2022-12-20 22:37:33 +0900

    chore: Spring Boot 프로젝트 환경 설정 추가
    - Spring Web Dependency 추가
    - lombok Dependency 추가
    - Spring Data Jpa Dependency 추가
    - MySQL Driver Dependency 추가

---
+++ src/main/java/com/seb41_pre_014/Seb41Pre014Application.java
@@ -0,0 +1,13 @@
+package com.seb41_pre_014;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb41Pre014Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb41Pre014Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/seb41_pre_014/Seb41Pre014ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb41_pre_014;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb41Pre014ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
