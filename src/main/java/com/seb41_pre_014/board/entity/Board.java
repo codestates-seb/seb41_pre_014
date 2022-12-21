1e4deefee605f3f36a11e652260b0e446a601197
jambin <gksmfcksqls@gmail.com>
2022-12-22 00:25:12 +0900

    feat - #4: Board, Vote 기본 Entity 구현
    Resolves: #4

---
+++ src/main/java/com/seb41_pre_014/board/entity/Board.java
@@ -0,0 +1,50 @@
+package com.seb41_pre_014.board.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+
+    private Long boardId;
+    private Long writerMemberId;
+    private String writerDisplayName;
+    private BoardType boardType;
+    private BoardStatus boardStatus;
+    private String title;
+    private String body;
+    private List<String> tags;
+    private int score = 0;
+    private int viewCount = 0;
+    private int bookmarkCount = 0;
+    private int answerCount = 0;
+    private Long questionId;
+
+    public enum BoardType {
+        QUESTION("Question"),
+        ANSWER("Anser");
+
+        private String type;
+
+        BoardType(String type) {
+            this.type = type;
+        }
+    }
+
+    public enum BoardStatus {
+        BOARD_PUBLIC("공개"),
+        BOARD_PRIVATE("임시저장"),
+        BOARD_DELETE("삭제");
+
+        private String status;
+
+        BoardStatus(String status) {
+            this.status = status; }
+    }
+}
