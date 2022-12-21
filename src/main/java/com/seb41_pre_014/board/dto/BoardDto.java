c401c82935384c1e7c3e06777e59731a9d05e4f3
jambin <gksmfcksqls@gmail.com>
2022-12-22 03:11:44 +0900

    feat - #11: Board, Vote DTO 기본 구현
    - BoardController postAnswer 메서드 추가
    Resolves: #11

---
+++ src/main/java/com/seb41_pre_014/board/dto/BoardDto.java
@@ -0,0 +1,46 @@
+package com.seb41_pre_014.board.dto;
+
+import com.seb41_pre_014.board.entity.Board;
+import lombok.*;
+
+import java.util.List;
+
+public class BoardDto {
+
+    @Getter
+    public static class Post {
+        private String writerDisplayName;
+        private String title;
+        private String body;
+        private List<String> tag;
+    }
+
+    @Getter
+    public static class Patch {
+        private String title;
+        private String body;
+        private List<String> tags;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Response {
+        private Long boardId;
+        private Long writerMemberId;
+        private Long writerReputation;
+        private Long writerProfileUrl;
+        private String writerDisplayName;
+        private Board.BoardType boardType;
+        private Board.BoardStatus boardStatus;
+        private String title;
+        private String body;
+        private List<String> tags;
+        private int score;
+        private int viewCount;
+        private int bookmarkCount;
+        private int answerCount;
+        private Long questionId;
+    }
+}
