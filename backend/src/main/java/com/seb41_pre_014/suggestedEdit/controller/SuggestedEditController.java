e22d23e9c341fff57c0987faf14be2fe28ad9531
Hionpu <parkpark011@gmail.com>
2022-12-26 00:58:01 +0900

    feat - #018: SugEdit, Tag DTO 유효성 검증

--- backend/src/main/java/com/seb41_pre_014/suggestedEdit/controller/SuggestedEditController.java
+++ backend/src/main/java/com/seb41_pre_014/suggestedEdit/controller/SuggestedEditController.java
@@ -1,12 +1,15 @@
 package com.seb41_pre_014.suggestedEdit.controller;
 
 
+import com.seb41_pre_014.board.dto.BoardDto;
 import com.seb41_pre_014.suggestedEdit.dto.SuggestedEditDto;
 import com.seb41_pre_014.suggestedEdit.entity.SuggestedEdit;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
@@ -14,19 +17,22 @@ import java.util.List;
 public class SuggestedEditController {
 
     @PostMapping
-    public ResponseEntity postEdit() {
+    public ResponseEntity postEdit(@PathVariable("edit-id") @Positive Long editId,
+                                   @RequestParam("memberId") @Positive Long memberId,
+                                   @RequestBody @Valid SuggestedEditDto.Post postEdit) {
         SuggestedEditDto.Response edit = stubEdit(1);
         return new ResponseEntity(edit, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{edits-id}")
-    public ResponseEntity patchEdit() {
+    public ResponseEntity patchEdit(@PathVariable("edit-id") @Positive Long editId,
+                                    @RequestBody @Valid SuggestedEditDto.Patch patchEdit) {
         SuggestedEditDto.Response edit = stubEdit(1);
         return new ResponseEntity(edit, HttpStatus.CREATED);
     }
 
     @GetMapping("/{edits-id}")
-    public ResponseEntity findEdit() {
+    public ResponseEntity findEdit(@PathVariable("edit-id") @Positive Long editId) {
         SuggestedEditDto.Response edit1 = stubEdit(1);
         SuggestedEditDto.Response edit2 = stubEdit(2);
         SuggestedEditDto.Response edit3 = stubEdit(3);
@@ -34,7 +40,8 @@ public class SuggestedEditController {
     }
 
     @GetMapping
-    public  ResponseEntity findAllEdit() {
+    public  ResponseEntity findAllEdit(@RequestParam(value = "page", defaultValue = "1") @Positive int page,
+                                       @RequestParam(value = "size", defaultValue = "20") @Positive int size) {
         SuggestedEditDto.Response edit1 = stubEdit(1);
         SuggestedEditDto.Response edit2 = stubEdit(2);
         SuggestedEditDto.Response edit3 = stubEdit(3);
@@ -49,6 +56,7 @@ public class SuggestedEditController {
                 .editId(n)
                 .body("Body of TempEdit" + n)
                 .tag(List.of("tag"+n,"tag2"+n+1))
+                .editStatus("처리중")
                 .build();
 
         return response;
