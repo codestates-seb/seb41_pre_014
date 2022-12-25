f5819c7b275dfbf0fb6c722b0204a4ddf7fa20e0
hionpu <111286364+hionpu@users.noreply.github.com>
2022-12-26 00:31:30 +0900

    feat: SuggestedEdit, Tag에 Stub 추가 (#54)
    * feat(entity생성): entity생성
    
    entity생성
    
    * feat(Added controller): Added controller
    
    Added controller
    
    * feat(Added controller): Added controller
    
    Added controller
    
    * feat(Added controller): Added controller
    
    Added controller
    
    * feat(Added controller): Added controller
    
    Added controller
    
    * feat(Added controller): Added controller
    
    Added controller
    
    * # Title Message
    # feat - #8: Added SugEdit, Tag controller
    # ex) feat - #123: 로그인 기능 추가
    ##### Subject 50 characters ################# -> |
    
    # 한 줄 공백. subject과 body 구분
    
    # Body Message (Optional)
    ######## Body 72 characters ####################################### -> |
    
    # Footer (Optional)
    # <type>: [#issueNumber-]
    # ex) Resolves: #123
    
    # --- COMMIT END ---
    # Title type can be
    #   feat    : 새로운 기능 추가
    #   fix     : 버그 수정
    #  design   : CSS등 사용자 UI 디자인 변경
    #  refactor : 코드 리팩토링
    #  comment  : 주석 추가 및 수정
    #   style   : 코드에 영향을 주지 않는 변경사항
    #   docs    : 문서 수정
    #   test    : 테스트 추가, 테스트 리팩토링
    #   chore   : 빌드 부분 혹은 패키지 매니저 수정사항
    #  rename   : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #  remove   : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    # Footer type can be
    #  Resolves : 이슈를 해결했을 때 사용(해당 이슈 닫음)
    #    Ref    : 참고할 이슈가 있을 때 사용
    # Related to: 해당 커밋에 관련된 이슈 번호
    # ------------------
    # Remember me ~
    #	subject는 어떤 작업을 했는지 명확하게 드러나야하고, 요약하여 작성합니다.
    #	subject는 명령조로 작성하며, 문장보다는 구문으로 작성합니다.
    #	영문으로 작성할 경우 첫글자는 대문자로 시작합니다.
    #	줄 끝에 마침표를 사용하지 않습니다.
    #	subject와 body 사이에 한 줄 띄어  구분합니다.
    #	body는 부연 설명이 필요한 경우 작성합니다. 선택사항입니다.
    #	body에서 어떻게 했는지가 아니라, 무엇을 왜 했는지를 작성합니다.
    #       body에 목록을 나타낼 때는 "-"로 시작합니다.
    #	footer는 이슈 번호를 참조시킬 때 주로 사용합니다. 선택사항입니다.
    
    * feat - #012: SugEdit, Tag Stub 추가

--- backend/src/main/java/com/seb41_pre_014/suggestedEdit/controller/SuggestedEditController.java
+++ backend/src/main/java/com/seb41_pre_014/suggestedEdit/controller/SuggestedEditController.java
@@ -1,29 +1,58 @@
 package com.seb41_pre_014.suggestedEdit.controller;
 
 
+import com.seb41_pre_014.suggestedEdit.dto.SuggestedEditDto;
+import com.seb41_pre_014.suggestedEdit.entity.SuggestedEdit;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/suggested-edits")
 public class SuggestedEditController {
 
-    @PostMapping("/{edits-id}")
+    @PostMapping
     public ResponseEntity postEdit() {
-        return new ResponseEntity(HttpStatus.CREATED);
+        SuggestedEditDto.Response edit = stubEdit(1);
+        return new ResponseEntity(edit, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{edits-id}")
+    public ResponseEntity patchEdit() {
+        SuggestedEditDto.Response edit = stubEdit(1);
+        return new ResponseEntity(edit, HttpStatus.CREATED);
     }
 
     @GetMapping("/{edits-id}")
     public ResponseEntity findEdit() {
-        return new ResponseEntity(HttpStatus.OK);
+        SuggestedEditDto.Response edit1 = stubEdit(1);
+        SuggestedEditDto.Response edit2 = stubEdit(2);
+        SuggestedEditDto.Response edit3 = stubEdit(3);
+        return new ResponseEntity(edit1, HttpStatus.OK);
     }
 
     @GetMapping
-    public  ResponseEntity findAllEdit() { return new ResponseEntity(HttpStatus.OK);}
+    public  ResponseEntity findAllEdit() {
+        SuggestedEditDto.Response edit1 = stubEdit(1);
+        SuggestedEditDto.Response edit2 = stubEdit(2);
+        SuggestedEditDto.Response edit3 = stubEdit(3);
+        return new ResponseEntity(List.of(edit1, edit2, edit3), HttpStatus.OK);}
+
+    public SuggestedEditDto.Response stubEdit(long n) {
+        SuggestedEditDto.Response.ResponseBuilder builder = SuggestedEditDto.Response.builder();
+        SuggestedEditDto.Response response = builder
+                .editId(n)
+                .title("TempEdit" + n)
+                .boardId(n)
+                .editId(n)
+                .body("Body of TempEdit" + n)
+                .tag(List.of("tag"+n,"tag2"+n+1))
+                .build();
+
+        return response;
+    }
 
 
 }
