5524b2b2ce25d8e9faf25862d494b28831afee53
hionpu <111286364+hionpu@users.noreply.github.com>
2022-12-26 00:23:30 +0900

    feat: SuggestedEdit, Tag Dto 추가 (#53)
    SuggestedEdit, Tag Dto 추가
    
    * # Title Message
    # feat - #8: Added SugEdit, Tag controller
    # ex) feat - #123: 로그인 기능 추가
    ##### Subject 50 characters ################# -> |
    
    # 한 줄 공백. subject과 body 구분
    
    # Body Message (Optional)
    ######## Body 72 characters ####################################### -> |
    
    # Footer (Optional)
    # <type>: [#issueNumber-]
    # ex) Resolves: #123
    
    # --- COMMIT END ---
    # Title type can be
    #   feat    : 새로운 기능 추가
    #   fix     : 버그 수정
    #  design   : CSS등 사용자 UI 디자인 변경
    #  refactor : 코드 리팩토링
    #  comment  : 주석 추가 및 수정
    #   style   : 코드에 영향을 주지 않는 변경사항
    #   docs    : 문서 수정
    #   test    : 테스트 추가, 테스트 리팩토링
    #   chore   : 빌드 부분 혹은 패키지 매니저 수정사항
    #  rename   : 파일 혹은 폴더명을 수정하거나 옮기는 작업만 하는 경우
    #  remove   : 파일을 삭제하는 작업만 수행한 경우
    # ------------------
    # Footer type can be
    #  Resolves : 이슈를 해결했을 때 사용(해당 이슈 닫음)
    #    Ref    : 참고할 이슈가 있을 때 사용
    # Related to: 해당 커밋에 관련된 이슈 번호
    # ------------------
    # Remember me ~
    #	subject는 어떤 작업을 했는지 명확하게 드러나야하고, 요약하여 작성합니다.
    #	subject는 명령조로 작성하며, 문장보다는 구문으로 작성합니다.
    #	영문으로 작성할 경우 첫글자는 대문자로 시작합니다.
    #	줄 끝에 마침표를 사용하지 않습니다.
    #	subject와 body 사이에 한 줄 띄어  구분합니다.
    #	body는 부연 설명이 필요한 경우 작성합니다. 선택사항입니다.
    #	body에서 어떻게 했는지가 아니라, 무엇을 왜 했는지를 작성합니다.
    #       body에 목록을 나타낼 때는 "-"로 시작합니다.
    #	footer는 이슈 번호를 참조시킬 때 주로 사용합니다. 선택사항입니다.
    
    * feat - #008
    
    * feat - #010: suggedit, tag dto

--- backend/src/main/java/com/seb41_pre_014/tag/dto/TagDto.java
+++ backend/src/main/java/com/seb41_pre_014/tag/dto/TagDto.java
@@ -1,4 +1,34 @@
 package com.seb41_pre_014.tag.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class TagDto {
+    @Getter
+    public static class post {
+        private String name;
+
+    }
+
+
+    @Getter
+    public static class patch {
+        private Long tagId;
+        private String name;
+        private Long boardId;
+    }
+
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+        private Long tagId;
+        private String name;
+        private Long boardId;
+
+    }
 }
