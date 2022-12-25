f188958b9fa0e90f1350fe77f911ead787248119
Hionpu <parkpark011@gmail.com>
2022-12-26 00:28:08 +0900

    feat - #012: SugEdit, Tag Stub 추가

--- backend/src/main/java/com/seb41_pre_014/tag/controller/TagController.java
+++ backend/src/main/java/com/seb41_pre_014/tag/controller/TagController.java
@@ -2,6 +2,7 @@ package com.seb41_pre_014.tag.controller;
 
 //<<<<<<< HEAD
 import com.seb41_pre_014.board.dto.BoardDto;
+import com.seb41_pre_014.suggestedEdit.dto.SuggestedEditDto;
 import com.seb41_pre_014.tag.dto.TagDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 //>>>>>>> origin/iss8
 
 @RestController
@@ -20,17 +23,40 @@ public class TagController {
 
     @PostMapping("/{tag-id}")
     public ResponseEntity postTag() {
-        return ResponseEntity.created(null).build();
+        TagDto.Response tag1 = stubTag(1);
+
+
+        return new ResponseEntity(tag1, HttpStatus.CREATED);
     }
+
+    // 태그는 통상 수정이 없고 무조건 새로운 태그가 생성되므로 patchTag는 없음
 
     @GetMapping("/{tag-id}")
     public ResponseEntity findTag() {
-        return ResponseEntity.ok().build();
+        TagDto.Response tag1 = stubTag(1);
+
+
+        return new ResponseEntity(tag1, HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity findAllTag() {
-        return ResponseEntity.ok().build();
+
+        TagDto.Response tag1 = stubTag(1);
+        TagDto.Response tag2 = stubTag(2);
+        TagDto.Response tag3 = stubTag(3);
+        return new ResponseEntity(List.of(tag1, tag2, tag3), HttpStatus.OK);
+    }
+
+    public TagDto.Response stubTag(long n) {
+        TagDto.Response.ResponseBuilder builder = TagDto.Response.builder();
+        TagDto.Response response = builder
+                .tagId(n)
+                .name("tag"+n)
+                .boardId(n)
+                .build();
+
+        return response;
     }
 
 
