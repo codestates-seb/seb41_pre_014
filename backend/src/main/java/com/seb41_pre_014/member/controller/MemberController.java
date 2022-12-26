0d933a0eb0a5e893b7b1a6b34db31b8768122590
TREMONTII <jimmy.jung0205@gmail.com>
2022-12-26 09:41:25 +0900

    feat - #13: Bookmark, Member Controller stub Data 구현

--- backend/src/main/java/com/seb41_pre_014/member/controller/MemberController.java
+++ backend/src/main/java/com/seb41_pre_014/member/controller/MemberController.java
@@ -1,7 +1,7 @@
 package com.seb41_pre_014.member.controller;
 
 
-
+import com.seb41_pre_014.member.dto.MemberDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +15,8 @@ public class MemberController {
 
     @PostMapping
     public ResponseEntity postMember() {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        MemberDto.Response createMember = createMember();
+        return new ResponseEntity<>(createMember, HttpStatus.CREATED);
     }
 
     @PatchMapping
@@ -31,6 +32,30 @@ public class MemberController {
     @DeleteMapping
     public ResponseEntity deleteMember() {
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    public MemberDto.Response createMember() {
+        MemberDto.Response.ResponseBuilder builder = MemberDto.Response.builder();
+        builder.memberId(1L);
+        builder.email("stub email@email.com");
+        builder.password("password");
+        builder.display_name("display_name");
+        builder.full_name("홍길동");
+        builder.location("home");
+        builder.title("자기소개");
+        builder.website_link("link1");
+        builder.twitter_link("link2");
+        builder.github_link("link3");
+        builder.profile_image_url("image url");
+        builder.memberstatus("활동중");
+        builder.reputation(10);
+        builder.questions(3);
+        builder.answers(3);
+        MemberDto.Response response;
+        response = builder.build();
+
+        return response;
     }
 
 }
