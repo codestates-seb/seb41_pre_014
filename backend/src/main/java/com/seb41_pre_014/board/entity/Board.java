42d4663426c53d7e71ab81b97a2ee86c752bcb9c
jambin <gksmfcksqls@gmail.com>
2022-12-27 15:57:45 +0900

    feat: Board, Membe, Vote, Bookmark 연관 관계 매핑

--- backend/src/main/java/com/seb41_pre_014/board/entity/Board.java
+++ backend/src/main/java/com/seb41_pre_014/board/entity/Board.java
@@ -1,12 +1,14 @@
 package com.seb41_pre_014.board.entity;
 
 import com.seb41_pre_014.audit.BaseTimeEntity;
+import com.seb41_pre_014.bookmark.entity.Bookmark;
+import com.seb41_pre_014.vote.entity.Vote;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,9 +20,18 @@ public class Board extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
+
+    @Column(nullable = false, updatable = false)
     private Long writerMemberId;
+
     private String writerDisplayName;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
     private BoardType boardType;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
     private BoardStatus boardStatus;
     private String title;
     private String body;
@@ -29,6 +40,12 @@ public class Board extends BaseTimeEntity {
     private int bookmarkCount = 0;
     private int answerCount = 0;
     private Long questionId;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<Bookmark> bookmarks = new ArrayList<>();
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<Vote> votes = new ArrayList<>();
 
     public enum BoardType {
         QUESTION("Question"),
