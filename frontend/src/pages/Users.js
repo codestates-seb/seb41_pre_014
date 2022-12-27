fa3ee2e7c9fa640efc31fab3dfbe0ba7f7f939b5
Juyeon Park <102612507+zoeypark@users.noreply.github.com>
2022-12-27 09:59:39 +0900

    feat - #45: Users UI 수정 및 Edit Profile 부분 구현 (#75)
    * feat - #45: Users UI 부분 구현
    
    * feat - #45: Users UI 수정 및 Edit Profile 부분 구현

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -1,3 +1,16 @@
-const Users = () => {};
+import { MainNavBar } from "../components/blocks/NavBar";
+import { UserMetaInfoType3 } from "../components/blocks/UserInfoContainer";
+import { UserEditForm, UserSettingsEditProfile } from "../components/blocks/UserSettingsEditProfile";
+
+
+const Users = () => {
+  return (
+    <div>
+      <UserMetaInfoType3/>
+      {/*edit profile 버튼*/}
+      <MainNavBar/>
+    </div>
+  )
+};
 
 export default Users;
