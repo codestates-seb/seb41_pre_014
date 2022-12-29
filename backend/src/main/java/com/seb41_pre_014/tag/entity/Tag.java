f7e27111cfde5bdcf410f044c1452a0bb9008fb3
jambin <gksmfcksqls@gmail.com>
2022-12-29 16:49:21 +0900

    feat - #88: Board, Member Controller 구현
    Related to: #88

--- backend/src/main/java/com/seb41_pre_014/tag/entity/Tag.java
+++ backend/src/main/java/com/seb41_pre_014/tag/entity/Tag.java
@@ -1,10 +1,7 @@
 package com.seb41_pre_014.tag.entity;
 
 import com.seb41_pre_014.board.entity.Board;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -14,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Setter
 @Entity
 public class Tag {
     @Id
