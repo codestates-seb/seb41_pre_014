29c8cac6f02123e03951f356d75e96e525a70263
Zoe Park <mary990t@gmail.com>
2022-12-29 16:00:52 +0900

    feat - #45: Input atom 수정

--- frontend/src/components/atoms/Input.js
+++ frontend/src/components/atoms/Input.js
@@ -2,17 +2,27 @@ import styled from 'styled-components';
 
 const StyledInput = styled.input`
   border: 0.1rem solid #BABFC4;
-
+  padding: ${props => props.padding || '0'};
+  width: ${props => props.width || '15rem'};
+  border-radius: ${props => props.borderRadius || '0.3rem'};
+  font-size: ${props => props.fontSize || '1.3rem'};
   &:focus {
     outline: 0.1rem solid #379fef;
     box-shadow: 0 0 0.5rem #b3d3ea;
   }
 `;
 
-export const Input = (placeholder) => {
+export const Input = (props) => {
   return (
     <>
-      <StyledInput placeholder={placeholder}></StyledInput>
+      <StyledInput 
+        placeholder={props.placeholder}
+        padding={props.padding}
+        width={props.width}
+        height={props.height}
+        borderRadius={props.borderRadius}
+        fontSize={props.fontSize}
+      ></StyledInput>
     </>
   )
 }
