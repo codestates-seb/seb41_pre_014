b699a49928406f3168bf4cbc5843a911c37d4730
jambin <gksmfcksqls@gmail.com>
2022-12-29 16:48:29 +0900

    fix: CORS 해결

---
+++ backend/src/main/java/com/seb41_pre_014/config/SecurityCorsConfig.java
@@ -0,0 +1,30 @@
+package com.seb41_pre_014.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityCorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("http://localhost:8081"); //특정패턴의 origin만 허용
+                config.addAllowedOriginPattern("http://localhost:3000");
+        config.addAllowedOriginPattern("http://localhost:8080");
+        config.addAllowedHeader(""); //특정 header만 허용
+        config.addAllowedMethod(""); //특정 메소드만 허용 //
+        config.addExposedHeader("Authorization");
+//        config.addExposedHeader("*"); //추가헤더,커스텀 헤더를 지정
+        source.registerCorsConfiguration("/**", config); //corsConfiguration으로 등록
+
+        return new CorsFilter(source);
+    }
+}
