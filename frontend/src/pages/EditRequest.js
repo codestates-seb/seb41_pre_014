c1d3743de4a21eb43ec075f09d47be6e0f2f796e
soojiin <skim69068@gmail.com>
2023-01-02 01:32:25 +0900

    feat - #101: Detail Edit Request 기능 구현

--- frontend/src/pages/EditRequest.js
+++ frontend/src/pages/EditRequest.js
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 import { NavItem } from '../components/atoms/NavItem';
 import { Button } from '../components/atoms/Button';
 import { TagBlock } from '../components/atoms/TagBlock';
@@ -48,7 +51,7 @@ const MainLeft = styled.div`
   > h3 {
     padding: 0.8rem 0;
 
-    > a {
+    > Link {
         font-size: 1.7rem;
         color: hsl(206deg 100% 40%);
         overflow-wrap: break-word;
@@ -61,6 +64,18 @@ const MainLeft = styled.div`
 const EditInfo = styled.div`
   display: flex;
   margin-top: 0.8rem;
+
+  .editInfoWrapper {
+    & > ul {
+    margin-bottom: 1.3rem;
+    list-style: none;
+
+      & > li {
+        display: inline-block;
+        margin-right: 0.4rem;
+      }
+    }
+  }
 
   .originalProposed {
     display: flex;
@@ -108,7 +123,7 @@ const TagHeader = styled.div`
   padding: 1.2rem 1.5rem;
 `;
 
-const TagContent = styled.div`
+const TagContent = styled.fieldset`
   display: flex;
   flex-direction: column;
   border-top: 0.1rem solid #e3e6e8;
@@ -139,7 +154,69 @@ const TagFooter = styled.div`
   padding: 1.2rem 1.5rem;
 `;
 
-const EditRequest = (props) => {
+const EditRequest = () => {
+  const navigate = useNavigate();
+  const [editRequest, SetEditRequest] = useState();
+  const [original, SetOriginal] = useState();
+
+  const getEditRequest = async (props) => {
+    try {
+      const response = await axios({
+        // editId, editorId ?
+        url: `/suggested-edits/${props.editId}`,
+        baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+      });
+      SetEditRequest(response.data);
+      console.log(editRequest);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const patchEditRequest = async (props) => {
+    try {
+      await axios({
+        method: "patch",
+        url: `/suggested-edits/${props.editId}`,
+        baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+        data: {
+          title: editRequest.title,
+          body: editRequest.body,
+          tags: editRequest.tags,
+        }
+      });
+      navigate(`/questions/${props.boardId}`)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const deleteEditRequest = async (props) => {
+    try {
+      await axios({
+        method: "delete",
+        url: `/suggested-edits/${props.editId}`,
+        baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+      });
+      navigate(`/questions/${props.boardId}`)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getOriginal = async (props) => {
+    try {
+      const response = await axios({
+        url: `/boards/${props.boardId}`,
+        baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+      });
+      SetOriginal(response.data);
+      console.log(original);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <>
       <MainTop>
@@ -168,22 +245,37 @@ const EditRequest = (props) => {
       <MainLeftRight>
         <MainLeft>
           <h2>Review the following question edit</h2>
-          <QuestionHorizontalMetaInfoWrapper />
-          <h3><a href={props.link}>{props.title}title</a></h3>
-          <UserMetaInfoType1 className='userMetaInfo' />
+          <QuestionHorizontalMetaInfoWrapper
+            votes={original.score}
+            answers={original.answerCount}
+            views={original.viewCount}
+          />
+          <h3><Link to={`/boards/${original.boardId}`}>{original.title}</Link></h3>
+          <UserMetaInfoType1
+            className='userMetaInfo'
+            profileImageUrl={original.writerProfileUrl}
+            displayName={original.writerDisplayName}
+            createdAt={original.createdAt}
+          />
           <EditInfo>
-            <LeftButtonWrapper />
+            <LeftButtonWrapper
+               score={original.score}
+            />
             <div className='editInfoWrapper'>
               <div className='originalProposed'>
-                <div>original{props.original}</div>
-                <div>proposed{props.proposed}</div>
+                <div>{original.body}</div>
+                <div>{editRequest.body}</div>
               </div>
-              {/* <div><TagBlock /></div> */}
+              <ul>
+                {editRequest.tags && editRequest.tags.map((el, idx) => {
+                  return <li key={idx}><TagBlock tagName={el} /></li>
+                })}
+              </ul>
               <div className='editorInfo'>
                 <div>Proposed</div>
-                <div>{props.time}time</div>
-                <div><img alt={props.username} src={props.avatarUrl} />avatar</div>
-                <div>{props.username}username</div>
+                <div>{editRequest.createdAt}</div>
+                <div><img alt={editRequest.editor} src={editRequest.editorProfileUrl} /></div>
+                <div>{editRequest.editor}</div>
               </div>
             </div>
           </EditInfo>
@@ -191,24 +283,46 @@ const EditRequest = (props) => {
         <MainRight>
           <TagContainer>
             <TagHeader>Actions</TagHeader>
-            <TagContent>
-              <label>
-                <input type='radio' name='actions' value='Approve' />
-                <span>Approve</span>
-              </label>
-              <label>
-                <input type='radio' name='actions' value='Reject' />
-                <span>Reject</span>
-              </label>
-            </TagContent>
-            <TagFooter>
-              <Button   
-                  buttonType='type2'buttonName='Submit'
-                  width='6.6rem' />
-              <Button 
-                buttonType='type4' buttonName='Skip'
-                width='5.2rem' />
-            </TagFooter>
+            <form>
+              <TagContent>
+                <label>
+                  <input type='radio' name='actions' value='Approve' />
+                  <span>Approve</span>
+                </label>
+                <label>
+                  <input type='radio' name='actions' value='Reject' />
+                  <span>Reject</span>
+                </label>
+              </TagContent>
+              <TagFooter>
+                <Button   
+                    buttonType='type2'
+                    buttonName='Submit'
+                    width='6.6rem'
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      e.target.actions.value === 'Approve' ? patchEditRequest({
+                        editId: editRequest.editId,
+                        boardId: editRequest.boardId
+                      }) : deleteEditRequest({
+                        editId: editRequest.editId,
+                        boardId: editRequest.boardId
+                      });
+                    }}
+                />
+                <Button 
+                  buttonType='type4' buttonName='Skip'
+                  width='5.2rem' 
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    deleteEditRequest({
+                      editId: editRequest.editId,
+                      boardId: editRequest.boardId
+                    });
+                  }}
+                />
+              </TagFooter>
+            </form>
           </TagContainer>
         </MainRight>
       </MainLeftRight>
