3703aa12e4147320f80cf8524858ee1bee337f61
Zoe Park <mary990t@gmail.com>
2023-01-02 22:01:38 +0900

    feat - #103: Edit Profile 버튼 기능 추가

--- frontend/src/components/blocks/UserDetailMainTop.js
+++ frontend/src/components/blocks/UserDetailMainTop.js
@@ -4,6 +4,7 @@ import { MainNavBar } from "./NavBar";
 import finalPropsSelectorFactory from "react-redux/es/connect/selectorFactory";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const MainTopWrapper = styled.div`
   display: flex;
@@ -19,29 +20,33 @@ const MainTopProfileWrapper = styled.div`
 `
 
 const MainTopProfile = (props) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/users/profile/settings');
+  }
   return (
     <MainTopProfileWrapper>
       <UserMetaInfoType3 
         displayName={props.displayName} 
         title={props.title} 
         location={props.location}/>
-      <EditProfileBtn>Edit Profile</EditProfileBtn>
+      <EditProfileBtn onClick={handleClick}>Edit Profile</EditProfileBtn>
     </MainTopProfileWrapper>
   );
 }
 
 const EditProfileBtn = styled.button`
   cursor: pointer;
-  font-size: 1.3rem;
+  font-size: 1.5rem;
   margin: 0.3rem;
   padding-left: 1.4rem;
   color: rgb(106, 115, 124);
-  width: 12rem;
-  height: 4rem;
+  width: 14rem;
+  height: 5rem;
   background-color: #fff;
   background: transparent url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="18" height="18" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"%3E%3Cpath fill="%236a737c" d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83l3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25Z"%2F%3E%3C%2Fsvg%3E')
   no-repeat 1.2rem center;
-  border-radius: 0.3rem;
+  border-radius: 0.5rem;
   border: 0.05rem solid #c3c7cc;
   &:hover {
     background-color: #f1f2f3;
